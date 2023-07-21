3e9bbcd7b03c47112e05da7f89ec469c8ea9f6ac
Lisa Knox <lisaknox83@gmail.com>
2023-07-20 23:16:42 -0400

    notif in title bar working

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
-document.body.style.border = '5px solid blue';
+document.body.style = 'border:3px solid red;';
 console.log('extension running');
+
+const updateTitle = (num) => {
+  const title = document.title;
+
+  const parens = /[()]/;
+  const parensContents = /\((.*?)\)/g;
+
+  document.title = parens.test(title)
+    ? title.replace(parensContents, `(${num})`)
+    : `(${num}) ${title}`;
+};
+
+let notifTracker = 0;
+setInterval(() => {
+  const numNotifs = document.querySelector('.ml-auto > text:nth-child(2)').firstChild.data * 1;
+  if (numNotifs !== notifTracker) {
+    notifTracker = numNotifs;
+    updateTitle(numNotifs);
+  }
+}, 5000);
