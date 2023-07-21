0de87c88cc4c99cea32239730c75f00a2b8b94cd
Lisa Knox <lisaknox83@gmail.com>
2023-07-21 12:26:09 -0400

    autofocus works except for rebugs

--- index.js
+++ index.js
@@ -1,21 +1,63 @@
 // console.log('extension running');
 
-const updateTitle = (num) => {
-  const title = document.title;
+//set notifications in browser tab----------------------
+const setNotifsInTitle = () => {
+  const updateTitle = (num) => {
+    const title = document.title;
 
-  const parens = /[()]/;
-  const parensContents = /\((.*?)\)/g;
+    const parens = /[()]/;
+    const parensContents = /\((.*?)\)/g;
 
-  document.title = parens.test(title)
-    ? title.replace(parensContents, `(${num})`)
-    : `(${num}) ${title}`;
+    document.title = parens.test(title)
+      ? title.replace(parensContents, `(${num})`)
+      : `(${num}) ${title}`;
+  };
+
+  let notifTracker = 0;
+  setInterval(() => {
+    const notifsElement = document.querySelector('.ml-auto > text:nth-child(2)');
+    if (notifsElement) {
+      const numNotifs = notifsElement.firstChild.data * 1;
+      if (numNotifs !== notifTracker) {
+        notifTracker = numNotifs;
+        updateTitle(numNotifs);
+      }
+    }
+  }, 5000);
 };
 
-let notifTracker = 0;
-setInterval(() => {
-  const numNotifs = document.querySelector('.ml-auto > text:nth-child(2)').firstChild.data * 1;
-  if (numNotifs !== notifTracker) {
-    notifTracker = numNotifs;
-    updateTitle(numNotifs);
-  }
-}, 5000);
+//focus first writable field
+
+const focusFirstWritable = () => {
+  const findAndFocus = () => {
+    const firstWritableField =
+      document.querySelector('textarea') || document.querySelector('input[type="text"]');
+    // document.querySelector('textarea.border-cherry') || document.querySelector('input.w-full');
+    if (firstWritableField) {
+      console.log(firstWritableField);
+      firstWritableField.focus();
+    }
+  };
+
+  // const allAnchors = document.querySelectorAll('a[title*="share"]');
+  // console.log(allAnchors);
+  // for (let i = 0; i < allAnchors.length; i++) {
+  //   allAnchors[i].addEventListener('click', findAndFocus);
+  // }
+
+  findAndFocus();
+};
+
+//for testing
+document.addEventListener('focusin', (event) => {
+  const focusedElement = event.target;
+  console.log('Element focused: ', focusedElement);
+  if (focusedElement.tagName !== 'TEXTAREA') focusFirstWritable();
+});
+
+const init = () => {
+  setNotifsInTitle();
+  focusFirstWritable();
+};
+
+init();
