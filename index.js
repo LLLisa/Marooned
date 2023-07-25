670f69f46d69de268eb07473b86c33cfaa993d98
Lisa Knox <lisaknox83@gmail.com>
2023-07-25 15:15:28 -0400

    sliced title hack

--- index.js
+++ index.js
@@ -1,63 +1,75 @@
 // console.log('extension running');
 
 //set notifications in browser tab----------------------
+const updateTitle = (newData) => {
+  const title = document.title.slice(document.title.indexOf('c'));
+
+  document.title = `${newData.notifs ? `(${newData.notifs})` : ''} ${
+    newData.inbox ? `[${newData.inbox}]` : ''
+  } ${title}`;
+};
+
 const setNotifsInTitle = () => {
-  const updateTitle = (num) => {
-    const title = document.title;
-
-    const parens = /[()]/;
-    const parensContents = /\((.*?)\)/g;
-
-    document.title = parens.test(title)
-      ? title.replace(parensContents, `(${num})`)
-      : `(${num}) ${title}`;
-  };
-
   let notifTracker = 0;
+  let inboxTracker = 0;
+
   setInterval(() => {
-    const notifsElement = document.querySelector('.ml-auto > text:nth-child(2)');
-    if (notifsElement) {
-      const numNotifs = notifsElement.firstChild.data * 1;
-      if (numNotifs !== notifTracker) {
-        notifTracker = numNotifs;
-        updateTitle(numNotifs);
-      }
+    const notifsElement = document.querySelector('a[href*="notifications"]');
+    const inboxElement = document.querySelector('a[href*="inbox"]');
+    const numNotifs = notifsElement
+      ? notifsElement.childNodes[0].innerText.replace('notifications', '') * 1
+      : null;
+
+    const numInbox = inboxElement
+      ? inboxElement.childNodes[0].innerText.replace('inbox', '') * 1
+      : null;
+    if (numNotifs !== notifTracker || numInbox !== inboxTracker) {
+      notifTracker = numNotifs;
+      inboxTracker = numInbox;
+      updateTitle({ notifs: numNotifs, inbox: numInbox });
     }
   }, 5000);
 };
 
 //focus first writable field
 
-const focusFirstWritable = () => {
-  const findAndFocus = () => {
-    const firstWritableField =
-      document.querySelector('textarea') || document.querySelector('input[type="text"]');
-    // document.querySelector('textarea.border-cherry') || document.querySelector('input.w-full');
-    if (firstWritableField) {
-      console.log(firstWritableField);
-      firstWritableField.focus();
-    }
-  };
+// const focusFirstWritable = () => {
+//   setTimeout(() => {
+//     getAllAnchors();
+//     const allInputs = [
+//       ...document.querySelectorAll('textarea'),
+//       ...document.querySelectorAll('input[type="text"]'),
+//     ];
+//     console.log('allInputs', allInputs);
 
-  // const allAnchors = document.querySelectorAll('a[title*="share"]');
-  // console.log(allAnchors);
-  // for (let i = 0; i < allAnchors.length; i++) {
-  //   allAnchors[i].addEventListener('click', findAndFocus);
-  // }
+//     const firstWritableField =
+//       document.querySelector('textarea') || document.querySelector('input[type="text"]');
+//     if (firstWritableField) {
+//       // console.log('firstWritableField', firstWritableField);
+//       firstWritableField.focus();
+//     }
+//   }, 100);
+// };
 
-  findAndFocus();
-};
+// // const getAllAnchors = async () => {
+// //   const allAnchors = document.querySelectorAll('a[title*="share"]');
+// //   await allAnchors.forEach((anchor) =>
+// //     anchor.addEventListener('click', () => {
+// //       console.log('click');
+// //       const allInputs = [
+// //         ...document.querySelectorAll('textarea'),
+// //         ...document.querySelectorAll('input[type="text"]'),
+// //       ];
+// //       console.log('allInputs', allInputs);
+// //     })
+// //   );
 
-//for testing
-document.addEventListener('focusin', (event) => {
-  const focusedElement = event.target;
-  console.log('Element focused: ', focusedElement);
-  if (focusedElement.tagName !== 'TEXTAREA') focusFirstWritable();
-});
+// //   console.log('allAnchors', allAnchors);
+// // };
 
 const init = () => {
   setNotifsInTitle();
-  focusFirstWritable();
+  // focusFirstWritable();
 };
 
 init();
